e8d8064980b878d92bb87c1b3ef22f25cd861779
Mark C <cookson.mark@gmail.com>
2022-04-13 10:05:07 +1200

    Add basic HTML buttons, script for test buttons

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const calculatorDisplay = document.getElementById("calculatorDisplay");
+
 const add = function(a, b) {
     return +a + +b;
 }
@@ -17,3 +19,32 @@ const divide = function(a, b) {
 const operate = function(operator, a, b) {
     return operator(a, b);
 }
+
+
+
+function updateDisplay(keypress) {
+    calculatorDisplay.textContent += keypress;
+}
+
+const button1 = document.getElementById("1")
+button1.addEventListener("click", function() {
+    updateDisplay('1');
+})
+
+const button2 = document.getElementById("2")
+button2.addEventListener("click", function() {
+    updateDisplay('2');
+})
+
+const buttonEquals = document.getElementById("equals")
+buttonEquals.addEventListener("click", function() {
+    updateDisplay('=');
+})
+
+const buttonPlus = document.getElementById("plus")
+buttonPlus.addEventListener("click", function() {
+    updateDisplay('+');
+})
+
+
+
