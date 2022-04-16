509867514a355567e65a402395e6baa769bbd6d8
Mark C <cookson.mark@gmail.com>
2022-04-16 18:43:31 +1200

    Implement decimal point, plus/minus and clearing display when new number entered after operator

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 const calculatorDisplay = document.getElementById("calculatorDisplay");
 
 const operators = ['add', 'subtract', 'multiply', 'divide']
+const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
 let displayValue = '0';
-let storedEntries = [0]
+let storedEntries = ['0']
 
 
 document.getElementById('buttons').addEventListener('click', event => {
@@ -11,24 +13,60 @@ document.getElementById('buttons').addEventListener('click', event => {
 
 
 function eventHandler(button) {
+    if (typeof displayValue === "number") { displayValue = displayValue.toString() }
+
     console.log('Event handler received button push: ' + button);
-    let storedEntry = storedEntries[storedEntries.length -1];
+
+    let previousEntry = storedEntries[storedEntries.length - 1];
+   
+    if (button === 'backspace') {
+        displayValue = displayValue.slice(0, -1);
+        if (displayValue === '') displayValue = '0';
+    }
 
     if (button === 'clear') {
         clear();
     }
 
+    if (button === 'point') {
+        if (!displayValue.includes('.')) displayValue += '.';
+    }
+
+    if (button === 'plusMinus') {
+        if (!displayValue.startsWith('-')) {
+            displayValue = '-' + displayValue;
+        }
+        else displayValue = displayValue.slice(1, );
+    }
+
+
+    if (digits.includes(button)) {
+        if (operators.includes(storedEntries[storedEntries.length -1])) {
+            displayValue = '0';
+            storedEntries.push(button)
+        }
+
+        if (displayValue == '0') {
+            displayValue = button;
+        }
+        else displayValue += button;
+    }
+
     if (operators.includes(button)) {
-        console.log('Event handler received the operator: ' + button)
+        storedEntries.push(displayValue);
+        storedEntries.push(button);
+
 
     }
+
+    calculatorDisplay.textContent = displayValue;
 }
 
 
 function clear() {
     console.log('Entered clear function');
-    let displayValue = 0;
-    let inputs = [0];
+    displayValue = 0;
+    inputs = [0];
 }
 
 
