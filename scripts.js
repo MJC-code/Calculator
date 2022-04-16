0499cf2b8a9a0f34dafaf5339c3b3b6d276e9773
Mark C <cookson.mark@gmail.com>
2022-04-16 17:47:57 +1200

    Check event.target.type so eventHandler is only called when click is inside a button of the buttons div

--- scripts.js
+++ scripts.js
@@ -1,28 +1,42 @@
 const calculatorDisplay = document.getElementById("calculatorDisplay");
 
 const operators = ['add', 'subtract', 'multiply', 'divide']
-let storedOperand = 0;
-let displayValue = 0;
-let expectingNewOperand = false;
-let operator = null;
-let previousOperator = null;
+let displayValue = '0';
+let storedEntries = [0]
+
 
 document.getElementById('buttons').addEventListener('click', event => {
-    eventHandler(event.target.id)
+    if (event.target.type === "button") eventHandler(event.target.id)
 })
 
 
 function eventHandler(button) {
     console.log('Event handler received button push: ' + button);
+    let storedEntry = storedEntries[storedEntries.length -1];
+
+    if (button === 'clear') {
+        clear();
+    }
+
+    if (operators.includes(button)) {
+        console.log('Event handler received the operator: ' + button)
+
+    }
 }
 
+
+function clear() {
+    console.log('Entered clear function');
+    let displayValue = 0;
+    let inputs = [0];
+}
 
 
 function operate(operator, a, b) {
     if (operator === 'add') return add(a, b);
     if (operator === 'subtract') return subtract(a, b);
     if (operator === 'multiply') return multiply(a, b);
-    if (operator === 'divide') return divide(a,b);
+    if (operator === 'divide') return divide(a, b);
     console.log('Error - Operator not recognised')
 }
 
